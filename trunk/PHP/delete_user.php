a476b4fa4d8206eee98514d812eeeb1398a77de7
daniel.kling <daniel.kling@12808416-1510-f6b6-9439-5891b00c85f2>
2013-03-17 14:37:32 +0000

    Deleting users works

--- trunk/PHP/delete_user.php
+++ trunk/PHP/delete_user.php
@@ -1,14 +1,22 @@
 <?php
+    require_once '../classes/database/MySqlDatabase.php';
+    
     $database = new MySqlDatabase();
     
     $database->open();
     
+    $address = $_GET["returnAddress"];
+    unset($_GET["returnAddress"]);
+    
     foreach ($_GET as $key => $value)
     {
-        $database->deleteUser($key);
+        $result = $database->deleteUser($key);
+        if (!$result)
+            echo "Failed to delete user!";
     }
     
     $database->close();
     
     unset($database);
+    header( "Location: $address");
 ?>
--- trunk/PHP/register_user.php
+++ trunk/PHP/register_user.php
@@ -1,5 +1,5 @@
 <?php
-    require_once './classes/database/MySqlDatabase.php';
+    require_once '../classes/database/MySqlDatabase.php';
     
     if (isset($_GET["isAdmin"]))
         $isAdmin = 1;
@@ -13,4 +13,9 @@
     $database->close();
     
     unset($database);
+
+    // Read the return address
+    $address = $_GET["returnAddress"];
+    // Return to previous page
+    header( "Location: $address");
 ?>

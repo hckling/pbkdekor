e2af70cc168325d6a3c54651835ce8c7ad5fbffd
daniel.kling <daniel.kling@12808416-1510-f6b6-9439-5891b00c85f2>
2013-03-17 14:37:32 +0000

    Deleting users works

--- administration.php
+++ administration.php
@@ -9,31 +9,42 @@ and open the template in the editor.
         <title>Administration</title>
     </head>
     <body>
-        <form name="createUser" action="register_user.php" method="get">
-            Förnamn: <input type="text" name="firstname"><br>
-            Efternamn: <input type="text" name="lastname"><br>
-            Email: <input type="text" name="email"><br>
-            Lösenord: <input type="password" name="password"><br>
-            Repetera lösenord: <input type="password" name="repeatedPassword"><br>
-            Admin: <input type="checkbox" name="isAdmin"><br>
-            <input type="submit" value="Skapa användare">
-        </form>
-        <?php
-            require_once './classes/database/MySqlDatabase.php';
-            
-            $database = new MySqlDatabase();
-            
-            $database->open();
-            $users = $database->getUsers();
-            $database->close();
-            
-            foreach ($users as $user)
-            {
-                echo $user->getHtml() . "<br>";
-                //unset($user);
-            }
-            
-            unset($database);
-        ?>
+        <p>
+            <form name="createUser" action="php\register_user.php" method="get">
+                Förnamn: <input type="text" name="firstname"><br>
+                Efternamn: <input type="text" name="lastname"><br>
+                Email: <input type="text" name="email"><br>
+                Lösenord: <input type="password" name="password"><br>
+                Repetera lösenord: <input type="password" name="repeatedPassword" label="Repetera lösenord"><br>
+                Admin: <input type="checkbox" name="isAdmin"><br>
+                <input type="hidden" name="returnAddress" value="../administration.php" visible="false"/>
+                <input type="submit" value="Skapa användare">
+            </form>
+        </p>
+        <p>
+            Existing users:<br>
+            <?php
+                require_once './classes/database/MySqlDatabase.php';
+
+                $database = new MySqlDatabase();
+
+                $database->open();
+                $users = $database->getUsers();
+                $database->close();
+
+                echo "<form name=\"deleteUser\" action=\"php/delete_user.php\" method=\"get\">";
+
+                foreach ($users as $user)
+                {
+                    $id = $user->getId();
+                    echo $user->getHtml() . " <input type=\"submit\" name=\"$id\" value=\"Radera\">" . "<br>";
+                    //unset($user);
+                }
+                echo "<input type=\"hidden\" name=\"returnAddress\" value=\"../administration.php\" visible=\"false\"/>";
+                echo "</form>";
+
+                unset($database);
+            ?>
+        </p>
     </body>
 </html>
